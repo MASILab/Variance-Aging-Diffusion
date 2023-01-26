d674c81ea419153acbd6c192829e000f9eb1929d
chenyugoal <chenyu_gao@outlook.com>
2023-01-25 22:57:09 -0600

    data preparation

--- lme.R
+++ lme.R
@@ -1,30 +1,21 @@
 library(readr)
+library(ggplot2)
 
 # Read csv
 brain_stats <- read_csv("/home/local/VANDERBILT/gaoc11/Projects/Variance-Aging-Diffusion/Data/BLSA_Brain_stats_concat_20221110_delivery.csv")
 motion_stats <- read_csv("/home/local/VANDERBILT/gaoc11/Projects/Variance-Aging-Diffusion/Data/BLSA_eddy_movement_rms_average_20221109.csv")
 
-# Select which to analyze
+################### DATA PREPARATION ###################
+# SELECT WHICH TO ANALYZE
 s_atlas = 'EveType1'
 s_region_id = '140'
 s_measure = 'FA_std'
 
-
 # Create DataFrame based on motion_stats
 df <- motion_stats[c("Subject_ID","Session","DTI_ID","Sex","Age","Motion")]
 
-# New column: Age at baseline visit
-df["Age_base"] <- NA
-
-list_subject <- unique(unlist(df$Subject_ID))
-
-for (sub in list_subject) {
-  age_baseline <- min(df[df$Subject_ID == sub, "Age"])
-  df[df$Subject_ID == sub, "Age_base"] <- age_baseline
-} 
-
-# New column: Interval (Age - Age_base)
-df['Interval'] <- df['Age'] - df['Age_base']
+# Drop the NA rows where Age info is missing
+df <- df[!is.na(df$Age),]
 
 # New column: selected measure
 df[s_measure] = NA
@@ -52,35 +43,55 @@ for (col in names(brain_stats)){
     } else if (grepl("DTI2", col, fixed=TRUE)) {
       dti_id = 2
     } else if (grepl("DTI_double", col, fixed=TRUE)) {
-      dti_id = 0
+      dti_id = NA
       next
     } else {
-      print("Error DTI_ID!")
+      print("Error: DTI_ID!")
     }
   } else {
   print("Error: measure type!")
   }
   
   if (atlas == s_atlas & region_id == s_region_id & measure == s_measure) {
-    
-    print(col)
-    print(c(atlas, region_id, measure, dti_id))
-    
     for (i in 1:nrow(brain_stats)) {
-      
       ses <- brain_stats['Session'][[1]][i]
       val <- brain_stats[col][[1]][i]
       
-      df[(df$Session == ses & df$DTI_ID ==dti_id), s_measure] <- val
+      df[(df$Session==ses)&(df$DTI_ID==dti_id), s_measure] <- val
     }
   }
 }
 
-nrow(df[!is.na(df$FA_std)&!is.na(df$Age),])
+# Drop the NA rows where measure value is missing
+df <- df[!is.na(df[s_measure]),]
 
-nrow(df)
-nrow(motion_stats)
-nrow(df[complete.cases(df),])
-summary(motion_stats)
-summary(df)
-View(df)
+# New column: Age at baseline visit
+df["Age_base"] <- NA
+
+list_subject <- unique(unlist(df$Subject_ID))
+
+for (sub in list_subject) {
+  age_baseline <- min(df[df$Subject_ID == sub, "Age"])
+  df[df$Subject_ID == sub, "Age_base"] <- age_baseline
+} 
+
+# New column: Interval, time since first visit
+df['Interval'] <- df['Age'] - df['Age_base']
+
+# Re-order and save
+df <- df[c("Subject_ID", "Sex", "Session", "DTI_ID", "Age", "Age_base", "Interval", "Motion", s_measure)]
+fn_save = sprintf("/home/local/VANDERBILT/gaoc11/Projects/Variance-Aging-Diffusion/Data/part/%s-%s-%s.csv", s_atlas, s_region_id, s_measure)
+write.csv(df, fn_save, row.names=FALSE, quote=FALSE)
+
+############################## TODO: Start from here
+# LINEAR MIXED EFFECTS MODEL
+# convert to factor class variables
+df <- within(df, {
+  Subject_ID <- factor(Subject_ID)
+  DTI_ID <- factor(DTI_ID)
+  Sex <- factor(Sex)
+})
+#
+p <- ggplot(data = df[df$DTI_ID==1,], aes(x = Age, y = FA_std, group = Subject_ID))
+p + geom_point()
+p + geom_line() + facet_grid(. ~ Sex)

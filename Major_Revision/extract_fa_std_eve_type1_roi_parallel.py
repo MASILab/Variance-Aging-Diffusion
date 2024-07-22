major revision: fix bug in merging single csvs, added signal-to-noise ratio calculation

Major_Revision/extract_fa_std_eve_type1_roi_parallel.py
@@ -11,8 +11,9 @@ def create_job_tuples(df, tmp_dir):
     tmp_dir = Path(tmp_dir)
 
     list_job_tuples = []
-    for idx, row in df.iterrows():            
-        wmatlas = Path(row['prequal_folder_updated'].replace('PreQual', 'WMAtlasEVE3'))
+    for idx, row in df.iterrows():
+        prequal = row['prequal_folder_updated']
+        wmatlas = Path(prequal.replace('PreQual', 'WMAtlasEVE3'))
         fa = wmatlas / "dwmri%fa.nii.gz"
         if fa.exists():
             save_csv = tmp_dir / f"{idx}.csv"
@@ -21,7 +22,7 @@ def create_job_tuples(df, tmp_dir):
             if save_csv.exists() and save_fa_seg.exists():
                 continue
             
-            list_job_tuples.append((wmatlas, save_csv, save_fa_seg))
+            list_job_tuples.append((prequal, wmatlas, save_csv, save_fa_seg))
         else:
             print(f"fa not found: {fa}")
     print(f"Total jobs: {len(list_job_tuples)}")
@@ -32,7 +33,7 @@ def create_job_tuples(df, tmp_dir):
 def extract_fa_std_eve_type1(job_tuple):
     global lut_csv, path_atlas_seg
     
-    wmatlas, save_csv, save_fa_seg = job_tuple
+    prequal, wmatlas, save_csv, save_fa_seg = job_tuple
     
     # Transform Eve Type 1 segmentation to FA space
     fa = wmatlas / 'dwmri%fa.nii.gz'
@@ -56,13 +57,18 @@ def extract_fa_std_eve_type1(job_tuple):
     data_seg = img_seg.get_fdata()
     data_fa = img_fa.get_fdata()
 
-    data = {}
+    data = {
+        'prequal_folder_updated': [prequal],
+        'wmatlas_folder': [str(wmatlas)],
+    }
     for roi_id in lut['id'].values:
         if np.sum(data_seg == roi_id) > 1:
             data[f"EveType1-{roi_id}-FA_std"] = [np.nanstd(data_fa[data_seg == roi_id])]
+            data[f"EveType1-{roi_id}-SNR"] = [np.nanmean(data_fa[data_seg == roi_id]) / np.nanstd(data_fa[data_seg == roi_id])]
         else:
-            print(f"ROI-{roi_id} has less than 2 voxels:\n{wmatlas}\n{save_fa_seg}\nUse NaN.")
+            print(f"Warning: ROI-{roi_id} has less than 2 voxels:\n{wmatlas}\n{save_fa_seg}\nUse NaN for FA_std and SNR")
             data[f"EveType1-{roi_id}-FA_std"] = [None]
+            data[f"EveType1-{roi_id}-SNR"] = [None]
     
     df = pd.DataFrame(data)
     df.to_csv(save_csv, index=False)
@@ -71,7 +77,8 @@ def extract_fa_std_eve_type1(job_tuple):
 def merge_csv(df_main, tmp_dir, final_csv):
     is_first = True
     for idx in df_main.index:
-        single_csv = tmp_dir / f"{idx}.csv"
+        single_csv = Path(tmp_dir) / f"{idx}.csv"
+
         if single_csv.exists():
             if is_first:
                 df = pd.read_csv(single_csv)
@@ -81,13 +88,14 @@ def merge_csv(df_main, tmp_dir, final_csv):
                 df = pd.concat([df, row], axis=0)                
         else:
             print(f"single_csv not found: {single_csv}")
-    df_main = pd.concat([df_main, df], axis=1)
+    
+    df_main = df_main.merge(df, on=['prequal_folder_updated'], how='left')
     df_main.to_csv(final_csv, index=False)
 
 
 if __name__ == '__main__':
 
-    # Parallel processing for extracting FA std from EVE Type 1 ROIs
+    # Parallel processing for extracting FA std and SNR from EVE Type 1 ROIs
     global lut_csv, path_atlas_seg
     lut_csv = '/home-local/gaoc11/fa_std_roi_eve3type1/input/EveType1_LUT.csv'
     path_atlas_seg = '/home-local/gaoc11/fa_std_roi_eve3type1/input/Atlas_JHU_MNI_SS_WMPM_Type-I.nii.gz'
@@ -96,9 +104,9 @@ if __name__ == '__main__':
     df = pd.read_csv('/home/local/VANDERBILT/gaoc11/Projects/Variance-Aging-Diffusion/Major_Revision/data/data_site_motion.csv')
     list_job_tuples = create_job_tuples(df, tmp_dir)
     
-    with Pool(processes=16) as pool:
+    with Pool(processes=10) as pool:
         for _ in tqdm(pool.imap(extract_fa_std_eve_type1, list_job_tuples, chunksize=1), total=len(list_job_tuples)):
             pass
 
     # Combine single csv to one
-    merge_csv(df, tmp_dir, '/home/local/VANDERBILT/gaoc11/Projects/Variance-Aging-Diffusion/Major_Revision/data/data_site_motion_fa-std.csv')
+    merge_csv(df, tmp_dir, '/home/local/VANDERBILT/gaoc11/Projects/Variance-Aging-Diffusion/Major_Revision/data/data_site_motion_fa-std_snr.csv')